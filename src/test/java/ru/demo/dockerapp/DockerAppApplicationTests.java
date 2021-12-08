e44c2039b35d427492c65ed3b2ec9c5fee0268fb
MaximPloskodniak <maximp@kmslh.com>
2021-12-08 09:57:49 +0300

    Initial commit

---
+++ src/test/java/ru/demo/dockerapp/DockerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.demo.dockerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
